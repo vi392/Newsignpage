b6fe1bc0cdcac66d81b9a9de1f9057c3cbe53a63
vi392 <vishalkrdwiwedi@gmail.com>
2023-09-01 17:53:54 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// var x= 5;
+// var y=7;
+// console.log(x+y);
+
+let person={
+    first_name: "vishal",
+    last_name: "Kumar",
+    age: 22
+};
+alert(person.first_name);
+console(person.last_name);
+document.write(person.first_name);
